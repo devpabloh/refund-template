488b3db352ec10b0d15c357e64900a7ac0b8e31a
Pablo Henrique <pablohenriqueadm94@gmail.com>
2024-11-20 23:49:07 -0300

    criando os itens da lista e a imagem deles de forma dinâmica, e também adicionamos eles a llista

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const amount = document.getElementById("amount")
 const expense = document.getElementById("expense")
 const category = document.getElementById("category")
 
+// Seleciona os elementos da lista
+const expenseList = document.querySelector("ul")
+
 // Capturando o evento de input para formatar o valor.
 amount.oninput = ()=>{
     // Obtém o valor atual do input e remove os caracteres não numéricos.
@@ -53,6 +56,14 @@ function expenseAdd(newExpense){
 
     // Cria o ícone da categoria
     const expenseIcon = document.createElement("img")
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`)
+    expenseIcon.setAttribute("alt", newExpense.category_name)
+
+    // Adiciona as informações no item.
+    expenseItem.append(expenseIcon)
+
+    // Adiciona o item na lista
+    expenseList.append(expenseItem)
     
         
     }catch(error){
